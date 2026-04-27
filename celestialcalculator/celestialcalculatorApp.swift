c38febd69c7b77a18ad608305c87877383ca2cbf
Dmytro Mayevsky <mayevskydv@gmail.com>
2026-04-27 12:56:33 +0200

    Initial Commit

---
+++ celestialcalculator/celestialcalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  celestialcalculatorApp.swift
+//  celestialcalculator
+//
+//  Created by Dmytro Mayevsky on 27.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct celestialcalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
